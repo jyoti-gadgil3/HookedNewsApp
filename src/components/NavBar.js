70e0f31136d6bf553797da55fe98981e3135c8c8
jyoti-gadgil3 <jyotig0699@gmail.com>
2025-03-10 14:50:04 +0530

    Updating class-based component to function-based

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
 import { Outlet, Link } from "react-router-dom";
 
-export class NavBar extends Component {
-
-    render() {
+const NavBar = () => {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,9 +11,7 @@ export class NavBar extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
-                            </li>
+                            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
@@ -33,7 +28,6 @@ export class NavBar extends Component {
                     <Outlet/>
             </div>
         )
-    }
 }
 
 export default NavBar
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageurl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="container my-3">
-        <div className="card">
-          <div style={{display: 'flex', justifyContent:'flex-end', position:'absolute', right:'0'}}>
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-          <img
-            src={
-              !imageurl
-                ? "https://imgs.hipertextual.com/wp-content/uploads/2024/07/es_disneyplus-breadth_c169_r_5a4.jpg"
-                : imageurl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title} </h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {author ? author : "Unknown"} on{" "}
-                {new Date(date).toUTCString()}
-              </small>
-            </p>
-            <a
-              href={newsUrl}
-              rel="noreferrer"
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+const NewsItem = (props) => {
+  let { title, description, imageurl, newsUrl, author, date, source } = props;
+  return (
+    <div className="container my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            !imageurl
+              ? "https://imgs.hipertextual.com/wp-content/uploads/2024/07/es_disneyplus-breadth_c169_r_5a4.jpg"
+              : imageurl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title} </h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author ? author : "Unknown"} on {new Date(date).toUTCString()}
+            </small>
+          </p>
+          <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
